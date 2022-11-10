f8240978c003d3e30ccf7a1ad3d335d290730c59
sudhir <sudhirtakale99@gmail.com>
2022-11-10 20:58:32 +0530

    Graph series

--- DSA_Practice/Graph/Adjancy_List.cpp
+++ DSA_Practice/Graph/Adjancy_List.cpp
@@ -1,32 +1,42 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-
-int main() {
-
-   int n, m,u,v;
-   cin >> n >> m;
-
-    list<int> adj[n +1];
-
-for (int i = 0; i < m; i++)
+int main()
 {
-    cin >> u >> v;
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-}
 
-for (int i = 0; i < m; i++)
-{
-    cout << i << "--->";
-    list<int>::iterator it;
-    for (it = adj[i].begin(); it != adj[i].end(); ++it)
+    int n, m, u, v;
+    cin >> n >> m;
+
+    vector<int> adj[n + 1];
+    cout << "Enter the values of m and n and edges of the graph:" << endl;
+    for (int i = 0; i < m; i++)
     {
-        cout << *it << " ";
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    cout << endl;
-}
+
+    // for (int i = 0; i < m; i++)
+    // {
+    //     cout << i << "--->";
+    //     list<int>::iterator it;
+    //     for (it = adj[i].begin(); it != adj[i].end(); ++it)
+    //     {
+    //         cout << *it << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < n + 1; j++)
+        {
+            cout << adj[i][j] << " ";
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
