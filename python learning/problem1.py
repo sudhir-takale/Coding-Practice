844a9a0cbf4ed6fddaddaf0a403254767c1fe79c
sudhir <sudhirtakale99@gmail.com>
2022-11-28 10:48:17 +0530

    python learning

---
+++ python learning/problem1.py
@@ -0,0 +1,12 @@
+num_one = 3
+num_two = 5
+
+for i in range(50):
+    if i /3 == 0:
+        print(i,"Divisible by 3")
+    elif i/5 == 0:
+        print(i,"divisible by 5")
+    
+
+    else :
+        print(i, "not divisible by any one")
