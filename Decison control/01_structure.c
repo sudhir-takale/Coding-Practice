b7c3eef39971816d0536caa90901d5647b0d7e55
sudhir <sudhirtakale99@gmail.com>
2022-12-13 19:03:17 +0530

    commit

--- Decison control/01_structure.c
+++ Decison control/01_structure.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
- struct book{
-     
-     char name[15];
-    
-     int pages;
-     float price;
-     char author[14];
+#include <stdio.h>
+struct book
+{
 
- }book; 
-int main(){
- printf("\nEnter the name of book  :");
- scanf("%s",&book.name[15]);
- printf("\nEnter the name of author of book :");
- scanf("%s", &book.author[14]);
-  printf("\nEnter the  of pages of book :");
- scanf("%d", &book.pages);
-printf("\nEnter the price of book  :");
- scanf("%f", &book.price);
+    char name[15];
 
-     return 0;
+    int pages;
+    float price;
+    char author[14];
+
+} book;
+int main()
+{
+    printf("\nEnter the name of book  :");
+    scanf("%s", &book.name[15]);
+    printf("\nEnter the name of author of book :");
+    scanf("%s", &book.author[14]);
+    printf("\nEnter the  of pages of book :");
+    scanf("%d", &book.pages);
+    printf("\nEnter the price of book  :");
+    scanf("%f", &book.price);
+
+    return 0;
 }
